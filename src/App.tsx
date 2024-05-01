22cd2da528009a6d90352558703ec3710f3b5995
Visakh Vijayan <visakh.vijayan@jalantechnologies.com>
2024-05-01 13:04:51 +0530

    added login and logout buttons using auth0

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Auth0LoginButton, Auth0LogoutButton } from "./components/index";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 className='text-8xl'>Vite + React</h1>
+      <h1 className="text-8xl">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -28,8 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <Auth0LoginButton onLogin={() => {}} />
+        <Auth0LogoutButton onLogout={() => {}} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
